f54a7e16c520e5029c5a8f988222723bd765d4b9
Krystian Kaszuba <krystian.kaszuba@student.pk.edu.pl>
2026-03-31 01:13:46 +0200

    First working version

---
+++ main.py
@@ -0,0 +1,74 @@
+import time
+
+import pyperclip
+import uvicorn
+from fastapi import FastAPI
+from plyer import notification
+from src.config import Config
+from src.brain import GPUModel
+from src.ui import AssistantUI
+from src.vision import NPUModel
+from src.visionOllama import GPU2Model
+from src.capture import WindowCapturer
+
+app = FastAPI()
+ui = AssistantUI() # rich console instance
+gpumodel = None
+npumodel = None
+capturer = None
+
+@app.get("/status")
+async def status():
+    return {"GPU status": "GPU " + config.models.gpu_model + " is online",
+            "NPU status": "NPU " + config.models.npu_model + " is online",
+            "CPU use": config.app.cpu_instead_npu
+            }
+
+@app.get("/")
+async def root():
+    return {"about", "blank"}
+
+@app.get("/captureAndAnalyze")
+async def captureanalyze():
+    pngpath = capturer.capture_active_window(config.app.screenshot_dir + time.ctime())
+    npureply = npumodel.message(pngpath)
+ #   ui.display_request(f"[NPU - reply] {npureply}")
+    gpureply = gpumodel.message(npureply)
+ #   ui.display_request(f"[GPU - reply] {gpureply}")
+    pyperclip.copy(gpureply[1:-1])
+    notification.notify(
+        title="Local Sensei",
+        message="Data has been copied into your clipboard",
+        app_name='LocalSensei',
+        timeout=5
+    )
+
+if __name__ == "__main__":
+    ui.display_request("[Startup] Loading config...")
+    config = Config.load()
+
+    ui.display_request(f"[Startup] Checking for Ollama's {config.models.gpu_model} model...")
+    gpumodel = GPUModel(config.models.gpu_model)
+
+    if config.app.use_twice_gpu:
+        ui.display_request(f"[Startup] Loading GPU model {config.models.gpu2_model}...")
+        npumodel = GPU2Model(config.models.gpu2_model)
+    else:
+        ui.display_request(f"[Startup] Loading {"CPU" if config.app.cpu_instead_npu else "NPU"} model...")
+        npumodel = NPUModel(config.models.npu_model, config.app.cpu_instead_npu)
+
+    ui.display_request(f"[Startup] Loading screen-shot system")
+    capturer = WindowCapturer()
+    uvicorn.run(
+        "src.main:app",
+        host="127.0.0.1",
+        port=8005,
+        reload=True,
+        log_level="info"
+    )
+    notification.notify(
+        title='Local Sensei',
+        message='System is ready to action',
+        app_name='LocalSensei',
+        timeout=5
+    )
